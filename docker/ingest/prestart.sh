8d8252ab5ec59884796d5675122421f5919afae6
Robert Hafner <tedivm@tedivm.com>
2023-01-01 23:53:30 -0600

    move migration to ingest container

--- docker/ingest/prestart.sh
+++ docker/ingest/prestart.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Fedimapper Ingester Prestart Script Running"
+
+echo "Run Database Migrations"
+python -m alembic upgrade head
--- docker/www/prestart.sh
+++ docker/www/prestart.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
 echo "FastAPI Prestart Script Running"
-
-echo "Run Database Migrations"
-python -m alembic upgrade head
-
